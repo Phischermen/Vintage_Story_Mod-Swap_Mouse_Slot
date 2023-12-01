ea82849bea8a0d07b1527c3789963e836eb5864c
Kevin Fischer <106684807+Kevin-Fischer-WSW@users.noreply.github.com>
2023-12-01 14:40:44 -0800

    Initial commit.

---
+++ SwapMouseSlot/InventoryUtil.cs
@@ -0,0 +1,84 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace VintageStoryMod1;
+
+public class InventoryUtil
+{
+    public static void SwapMouseSlotAndActiveHotbarSlot(ICoreClientAPI capi)
+    {
+        var player = capi.World.Player;
+        var im = player.InventoryManager;
+        var mSlot = im.MouseItemSlot;
+        var aSlot = im.ActiveHotbarSlot;
+        ItemSlot tSlot = null;
+
+        if (mSlot == null || aSlot == null)
+        {
+            return;
+        }
+
+        if (mSlot.Empty && aSlot.Empty)
+        {
+            return;
+        }
+
+        if (mSlot.Empty == false && aSlot.Empty)
+        {
+            TryTransferTo(mSlot, aSlot);
+        }
+        else if (aSlot.Empty == false && mSlot.Empty)
+        {
+            TryTransferTo(aSlot, mSlot);
+        }
+        else
+        {
+            // Note: tSlot will be assigned in Matcher
+            var find = im.Find(Matcher);
+
+            if (!find)
+            {
+                // Show the player a message
+                capi.ShowChatMessage("No empty slot found!");
+                return;
+            }
+
+            TryTransferTo(mSlot, tSlot);
+            TryTransferTo(aSlot, mSlot);
+            TryTransferTo(tSlot, aSlot);
+        }
+        mSlot.MarkDirty();
+        aSlot.MarkDirty();
+        tSlot?.MarkDirty();
+        
+        return;
+
+        bool Matcher(ItemSlot slot)
+        {
+            if (slot == null)
+            {
+                return false;
+            }
+            var b1 = slot != mSlot && slot != aSlot;
+            var b2 = !slot.Inventory.InventoryID.StartsWith("ground");
+            var b3 = slot.Empty;
+            var b4 = slot.CanHold(mSlot);
+            if (b1 && b2 && b3 && b4)
+            {
+                tSlot = slot;
+                return true;
+            }
+
+            return false;
+        }
+
+        void TryTransferTo(ItemSlot source, ItemSlot target)
+        {
+            ItemStackMoveOperation op =
+                new ItemStackMoveOperation(capi.World, EnumMouseButton.Left, 0, EnumMergePriority.AutoMerge,
+                    source.Itemstack?.StackSize ?? 0);
+            var packet = im.TryTransferTo(source, target, ref op);
+            capi.Network.SendPacketClient(packet);
+        }
+    }
+}
---
+++ SwapMouseSlot/VintageStoryMod1ModSystem.cs
@@ -0,0 +1,40 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace VintageStoryMod1
+{
+    public class VintageStoryMod1ModSystem : ModSystem
+    {
+        private ICoreClientAPI capi;
+
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return forSide == EnumAppSide.Client;
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+
+            capi = api;
+            capi.Input.RegisterHotKey("swapmouseitemslot", "Swap Mouse And Active Hotbat Slot", GlKeys.X,
+                HotkeyType.InventoryHotkeys);
+            capi.Input.SetHotKeyHandler("swapmouseitemslot", Handler);
+        }
+
+        private bool Handler(KeyCombination t1)
+        {
+            try
+            {
+                InventoryUtil.SwapMouseSlotAndActiveHotbarSlot(capi);
+            }
+            catch (System.Exception e)
+            {
+                capi.Logger.Error(e);
+                capi.World.Player.ShowChatNotification("An eldritch horror is keeping you from swapping items!");
+            }
+
+            return true;
+        }
+    }
+}
